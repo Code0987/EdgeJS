035919ab6bd0e09f2e1101cbf6d40ce93ee510d1
Code0987 <durgapalneeraj@gmail.com>
2014-08-17 14:51:43 +0530

    #176 applied.

--- Test/WPFTest/EdgeTest/bin/Debug/edge.app/app.js
+++ Test/WPFTest/EdgeTest/bin/Debug/edge.app/app.js
@@ -1,25 +1,4 @@
-﻿// FIX: #176 @https://github.com/tjanczuk/edge/issues/176 {
-try {
-    var stdout = process.stdout;
-}
-catch (e) {
-    // This is a Windows GUI application without stdout and stderr defined.
-    // Define process.stdout and process.stderr so that all output is discarded.
-    (function () {
-        var stream = require('stream');
-        var NullStream = function (o) {
-            stream.Writable.call(this);
-            this._write = function (c, e, cb) { cb && cb(); };
-        }
-        require('util').inherits(NullStream, stream.Writable);
-        var nullStream = new NullStream();
-        process.__defineGetter__('stdout', function () { return nullStream; });
-        process.__defineGetter__('stderr', function () { return nullStream; });
-    })();
-}
-// }
-
-module.exports = function (data, callback) {
+﻿module.exports = function (data, callback) {
 
     // Imports.
     var edge = require("./../edge/edge.js"),
